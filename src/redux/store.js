8b667ce5c3a64d18fad881d1eec7cae14f65d33f
mousahmed <mous.ahmed@outlook.com>
2021-11-04 14:34:42 +0200

    implemented redux sagas in the application and modified shop reducer and component to use sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./root-reducer";
+import rootSaga from "./root-saga";
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
 	middlewares.push(logger);
@@ -13,5 +16,6 @@ if (process.env.NODE_ENV === "development") {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export const persistor = persistStore(store);
+sagaMiddleware.run(rootSaga);
 
+export const persistor = persistStore(store);
