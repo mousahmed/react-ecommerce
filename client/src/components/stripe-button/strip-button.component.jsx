a050d49b17d45aaf252f8a0c86d7f535a7aae6c7
mousahmed <mous.ahmed@outlook.com>
2021-12-02 15:55:30 +0200

    seperationg react app into client folder, added express server to handle backend stripe payment

--- client/src/components/stripe-button/strip-button.component.jsx
+++ client/src/components/stripe-button/strip-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
 	const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
 		"pk_test_51JpvQrFK30aQTUZhDbcVlHVf3b1gHkdO0nRBUKmfZXXfl7XeReC8PpJZfKvOptlVPnP7ywrsZfx9doZ9omIPWQuq00VDXXhebV";
 
 	const onToken = (token) => {
-		console.log(token);
-		alert("Payment Successful");
+		axios({
+			url: "payment",
+			method: "post",
+			data: {
+				amount: priceForStripe,
+				token: token,
+			},
+		})
+			.then((response) => {
+				alert("succesful payment");
+			})
+			.catch((error) => {
+				console.log("Payment Error: ", error);
+				alert(
+					"There was an issue with your payment! Please make sure you use the provided credit card."
+				);
+			});
 	};
 
 	return (
@@ -17,7 +33,7 @@ const StripeCheckoutButton = ({ price }) => {
 			name="React Clothing Store"
 			billingAddress
 			shippingAddress
-			image='https://svgshare.com/i/CUz.svg'
+			image="https://svgshare.com/i/CUz.svg"
 			description={`Your total is $${price}`}
 			amount={priceForStripe}
 			panelLabel="Pay Now"
