0b65747b0eedcba7b4fd3569bab4c85feac6e4fc
Moustafa Mansour <mous.ahmed@outlook.com>
2023-02-17 13:46:20 +0200

    Created addToCart Feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,15 @@ import { useContext } from "react";
 import { CartContext } from "contexts/cart.context";
 
 function CartIcon() {
-  const { setToggleCart } = useContext(CartContext);
+  const { totalQuantity, setToggleCart } = useContext(CartContext);
+
   return (
     <div
       className="cart-icon-container"
       onClick={() => setToggleCart((prev) => !prev)}
     >
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{totalQuantity}</span>
     </div>
   );
 }
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,46 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (item) => productToAdd.id === item.id,
+  );
+  if (existingCartItem) {
+    existingCartItem.quantity += 1;
+    return [...cartItems];
+  }
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
   toggleCart: false,
-  setToggleCart: () => null,
+  setToggleCart: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  totalQuantity: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [toggleCart, setToggleCart] = useState(false);
-  const value = { toggleCart, setToggleCart };
+  const [cartItems, setCartItems] = useState([]);
+  const [totalQuantity, setTotalQuantity] = useState(0);
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => (total += cartItem.quantity),
+      0,
+    );
+    setTotalQuantity(newCartCount);
+  }, [cartItems]);
+  const value = {
+    toggleCart,
+    setToggleCart,
+    cartItems,
+    addItemToCart,
+    totalQuantity,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
